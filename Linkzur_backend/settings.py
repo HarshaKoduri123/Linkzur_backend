44e3e72bd6c0ca7cc7493695f57432822b35635d
HarshaKoduri123 <harsha.koduri123@gmail.com>
2025-10-05 14:18:48 -0400

    Deploy to Render

--- Linkzur_backend/settings.py
+++ Linkzur_backend/settings.py
@@ -16,8 +16,7 @@ ALLOWED_HOSTS = [
 
 # ✅ CORS setup
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8081",
+
 ]
 CORS_ALLOW_CREDENTIALS = True  # optional but often useful
 
