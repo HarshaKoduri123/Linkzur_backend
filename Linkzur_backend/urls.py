2466235e40be09ac592ebe6f234020d9ca28fc27
HarshaKoduri123 <harsha.koduri123@gmail.com>
2025-10-08 02:33:33 -0400

    Deploy to Render

--- Linkzur_backend/urls.py
+++ Linkzur_backend/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-import os
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -10,11 +9,10 @@ urlpatterns = [
     path('api/', include('linkzur_app.urls')),
 ]
 
-# Only serve React build in production (DEBUG=False)
-if not settings.DEBUG:
-    urlpatterns += [re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))]
-
-# Serve media in dev
+# ✅ Serve static + media files (in dev only)
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# ✅ Add this LAST so it doesn’t override static
+urlpatterns += [re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))]
